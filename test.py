a3051db8a9a1ae39228855f59be8074b2c7f982c
Alessio Margelli <alex9446@gmail.com>
2021-03-06 16:30:39 +0100

    🖇️ Integrated the default mechanism of flask tests

--- test.py
+++ test.py
@@ -2,37 +2,38 @@ import unittest
 from base64 import b64decode, b64encode
 from json import dumps, loads
 
-import requests
-
+from server import app
 from src.parameters import get_parameter
 
-
-def local_server() -> str:
-    return 'http://localhost:' + get_parameter('port')
+app.config['TESTING'] = True
 
 
 class Test(unittest.TestCase):
     @staticmethod
     def set_login() -> dict:
-        return requests.post(
-            url=local_server() + '/login',
-            json={
-                'name': 'admin',
-                'password': get_parameter('first_admin_password')
-            }
-        )
+        with app.test_client() as client:
+            response = client.post(
+                '/login',
+                json={
+                    'name': 'admin',
+                    'password': get_parameter('first_admin_password')
+                }
+            )
+        return response
 
     @staticmethod
     def get_login(token: str) -> dict:
-        return requests.get(
-            url=local_server() + '/login',
-            headers={'Authorization': 'Bearer ' + token}
-        )
+        with app.test_client() as client:
+            response = client.get(
+                '/login',
+                headers={'Authorization': 'Bearer ' + token}
+            )
+        return response
 
     @staticmethod
     def alterate_token(token: str) -> str:
         t1, t2, t3 = token.split('.')
-        payload_dict = loads(b64decode(t2 + '==='))
+        payload_dict = loads(b64decode(t2 + '===').decode('utf-8'))
         payload_dict['level'] = 'manager'
         t2 = b64encode(dumps(payload_dict).encode('utf-8')).decode('utf-8')
         return f'{t1}.{t2}.{t3}'
@@ -40,26 +41,27 @@ class Test(unittest.TestCase):
     def test_login(self):
         response = self.set_login()
         self.assertEqual(response.status_code, 200)
-        response_dict = response.json()
+        response_dict = response.get_json()
         self.assertEqual(response_dict['status'], 'ok')
         self.assertIsInstance(response_dict['token'], str)
-        self.assertTrue(response_dict['token'])
+        self.assertTrue(len(response_dict['token']) > 0)
 
     def test_login_info(self):
-        response = self.get_login(self.set_login().json()['token'])
+        response = self.get_login(self.set_login().get_json()['token'])
         self.assertEqual(response.status_code, 200)
-        response_dict = response.json()
+        response_dict = response.get_json()
         self.assertEqual(response_dict['status'], 'ok')
         self.assertIsInstance(response_dict['jwt_payload'], dict)
         self.assertEqual(response_dict['jwt_payload']['name'], 'admin')
 
     def test_altered_token(self):
-        token = self.alterate_token(self.set_login().json()['token'])
+        token = self.alterate_token(self.set_login().get_json()['token'])
         response = self.get_login(token)
         self.assertEqual(response.status_code, 401)
-        response_dict = response.json()
+        response_dict = response.get_json()
         self.assertEqual(response_dict['status'], 'error')
-        self.assertIsInstance(response_dict['message'], str)
+        self.assertEqual(response_dict['message'],
+                         'Incorrect account credentials!')
 
 
 if __name__ == '__main__':
